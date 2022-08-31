dcd4f14a0260151999863333360f1a4acda58da3
konstantin <kosvadmail@atomlc.com.ua>
2022-08-31 18:00:16 +0200

    Добавляет кликабельную форму редактирования точки путешествия

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -6,32 +6,59 @@ import NewTripPointOfferView from '../view/new-trip-point-offer-view';
 import NewTripPointDestinationView from '../view/new-trip-point-destination-view.js';
 import TripPointEditView from '../view/trip-point-edit-view.js';
 
-import {render, RenderPosition} from '../render.js';
-import {COUNT_OF_TRIP_POINTS} from '../constants.js';
+import {
+  render,
+  RenderPosition
+} from '../render.js';
+import {
+  COUNT_OF_TRIP_POINTS
+} from '../constants.js';
 
 export default class TripPresenter {
   tripPointsComponent = new TripPointsContainerView();
   tripPointSortComponent = new TripPointsSortView();
   tripPointCreatorComponent = new NewTripPointView();
-  tripPointEditorComponent = new TripPointEditView();
 
   init(tripMainContainer, TripPointsData) {
     this.tripMainContainer = tripMainContainer;
     render(this.tripPointsComponent, this.tripMainContainer);
     render(this.tripPointSortComponent, this.tripMainContainer, RenderPosition.AFTERBEGIN);
 
-    render(this.tripPointCreatorComponent, this.tripPointsComponent.getElement());
+    for (let i = 0; i < COUNT_OF_TRIP_POINTS; i++) {
+      const newTrip = new TripPointView(TripPointsData[i]);
+      const tripPointEditorComponent = new TripPointEditView();
+      const pointEditorOpenerElement = newTrip.getElement().querySelector('.event__rollup-btn');
+      const pointEditorCloserElement = tripPointEditorComponent.getElement().querySelector('.event__rollup-btn');
+      const pointEditorFormElement = tripPointEditorComponent.getElement().querySelector('.event--edit');
 
-    for(let i = 0; i < COUNT_OF_TRIP_POINTS; i++) {
-      render(new TripPointView(TripPointsData[i]), this.tripPointsComponent.getElement());
+      render(newTrip, this.tripPointsComponent.getElement());
+
+      render(new NewTripPointOfferView(), tripPointEditorComponent.getElement().querySelector('.event__details'), RenderPosition.AFTERBEGIN);
+      render(new NewTripPointDestinationView(), tripPointEditorComponent.getElement().querySelector('.event__details'));
+
+      const onEscKeyDown = (evt) => {
+        if (evt.key === 'Escape' || evt.key === 'Esc') {
+          evt.preventDefault();
+          tripPointEditorComponent.getElement().replaceWith(newTrip.getElement());
+          document.removeEventListener('keydown', onEscKeyDown);
+        }
+      };
+
+      pointEditorOpenerElement.addEventListener('click', () => {
+        newTrip.getElement().replaceWith(tripPointEditorComponent.getElement());
+        document.addEventListener('keydown', onEscKeyDown);
+      });
+
+      pointEditorCloserElement.addEventListener('click', () => {
+        tripPointEditorComponent.getElement().replaceWith(newTrip.getElement());
+        document.removeEventListener('keydown', onEscKeyDown);
+
+      });
+      pointEditorFormElement.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        tripPointEditorComponent.getElement().replaceWith(newTrip.getElement());
+        document.removeEventListener('keydown', onEscKeyDown);
+      });
     }
-
-    render(this.tripPointEditorComponent, this.tripPointsComponent.getElement().lastElementChild);
-
-    render(new NewTripPointOfferView(), this.tripPointCreatorComponent.getElement().querySelector('.event__details'), RenderPosition.AFTERBEGIN);
-    render(new NewTripPointDestinationView(), this.tripPointCreatorComponent.getElement().querySelector('.event__details'));
-
-    render(new NewTripPointOfferView(), this.tripPointEditorComponent.getElement().querySelector('.event__details'), RenderPosition.AFTERBEGIN);
-    render(new NewTripPointDestinationView(), this.tripPointEditorComponent.getElement().querySelector('.event__details'));
   }
 }
